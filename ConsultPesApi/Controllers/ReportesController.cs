febb843bf40707da02ebb7cf59c57894833a81bc
tuhdanger002 <evg.wilfredbelliard@gmail.com>
2026-01-16 14:12:12 -0400

    Halar datos pentiendes

--- ConsultPesApi/Controllers/ReportesController.cs
+++ ConsultPesApi/Controllers/ReportesController.cs
@@ -37,11 +37,15 @@ public class ReportesController : ControllerBase
       [FromQuery] DateTime? fechaInicio,
       [FromQuery] DateTime? fechaFin)
     {
+        if (fechaFin.HasValue)
+        {
+            fechaFin = fechaFin.Value.Date.AddHours(23).AddMinutes(59).AddSeconds(59);
+        }
         using (var connection = new SqlConnection(_connectionString))
         {
             // Ajustamos el WHERE para que el BETWEEN sea estricto si hay fechas
             var sql = @"
-            SELECT top 100 * FROM Reporte_Pes_Detalle
+            SELECT * FROM SYSTEMPES.DBO.Reporte_Pes_Detalle
             WHERE (@numPes IS NULL OR NUM_PES = @numPes)
             AND (@usuario IS NULL OR USUARIO LIKE '%' + @usuario + '%')
             AND (@sector IS NULL OR SECTOR = @sector)
@@ -66,4 +70,49 @@ public class ReportesController : ControllerBase
     }
 
 
+
+
+    [HttpGet("buscar2")]
+    public async Task<IActionResult> GetFiltrado2(
+        [FromQuery] int? numPes,
+        [FromQuery] string? usuario,
+        [FromQuery] string? sector,
+        [FromQuery] string? gerencia,
+        [FromQuery] DateTime? fechaInicio,
+        [FromQuery] DateTime? fechaFin)
+    {
+        // Ajustamos fechaFin para que sea 23:59:59 del día elegido
+        DateTime? fechaFinFull = fechaFin?.Date.AddHours(23).AddMinutes(59).AddSeconds(59);
+
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            // T1: PES_ACTIVOS_SGD01 (Origen)
+            // T2: Reporte_Pes_Detalle (Destino/Exclusión)
+            var sql = @"
+            SELECT T1.* FROM SYSTEMPES02.DBO.vw_PES_ACTIVOS_SGD01 T1
+            LEFT JOIN SYSTEMPES.DBO.Reporte_Pes_Detalle T2 ON T1.NUM_PES = T2.NUM_PES AND  DATEPART(YEAR,T1.FECHA_INI) = DATEPART(YEAR,T2.FECHA_INICIO) 
+            WHERE T2.NUM_PES IS NULL
+              AND (@numPes IS NULL OR T1.NUM_PES = @numPes)
+              AND (@usuario IS NULL OR T1.USUARIO LIKE '%' + @usuario + '%')
+              AND (@sector IS NULL OR T1.SECTOR = @sector)
+              AND (@gerencia IS NULL OR T1.sector = @gerencia)
+              AND (
+                  (@fechaInicio IS NULL OR T1.FECHA_INI >= @fechaInicio)
+                  AND 
+                  (@fechaFinFull IS NULL OR T1.FECHA_INI <= @fechaFinFull)
+              )";
+
+            var resultados = await connection.QueryAsync<dynamic>(sql, new
+            {
+                numPes,
+                usuario,
+                sector,
+                gerencia,
+                fechaInicio,
+                fechaFinFull = fechaFinFull // Usamos la fecha con la hora ajustada
+            });
+
+            return Ok(resultados);
+        }
+    }
 }
